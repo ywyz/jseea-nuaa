12f3b136b968fe122b8a3287b09049bbbc961f3d
ywyz <admin@ywyz.tech>
2022-03-21 21:01:19 +0800

    添加C++报告，配置C++环境

---
+++ C_plus/test.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Date: 2022-03-21 18:59:00
+ * @Author: ywyz
+ * @LastModifiedBy: ywyz
+ * @Github: https://github.com/ywyz
+ * @LastEditors: ywyz
+ * @LastEditTime: 2022-03-21 20:53:05
+ */
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Hello World";
+    return 0;
+}
