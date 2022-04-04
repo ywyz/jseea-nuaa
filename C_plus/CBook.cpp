19205edec35c068613b75bd1217b3f56f72b6a9b
ywyz <admin@ywyz.tech>
2022-04-04 20:18:20 +0800

    更新程序

---
+++ C_plus/CBook.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yw980 on 2022/4/4.
+//
+
+#include "CBook.h"
---
+++ C_plus/CBook.h
@@ -0,0 +1,16 @@
+//
+// Created by yw980 on 2022/4/4.
+//
+
+#ifndef CBOOK_H
+#define CBOOK_H
+#include <string>
+using namespace std;
+class CBook {
+private:
+    int id;         // Õº È±‡∫≈
+    string BookName;    //
+};
+
+
+#endif //C_PLUS_CBOOK_H
